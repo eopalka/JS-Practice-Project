66c8d9897e7116aca78109af1b163ddb1285ff74
elisa <elisaopalka@gmail.com>
2021-02-18 13:25:22 -0500

    added link functions

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,12 @@
-const blogs = [];
+const blogs = [
+    {title: "My First Blog",content: "omg"},
+    {title: "My Second Blog",content: "yass"},
+    {title: "My Third Blog",content: "omg no"},
+    {title: "My Last Blog",content: "wtf"},
+];
+
+
+
 
 function main() { 
     return document.getElementById("main");
@@ -15,6 +23,18 @@ function contentInput() {
 function form() {
     return document.getElementById("form");
 }
+
+function formLink() {
+    return document.getElementById("form-link")
+}
+
+function blogsLink() {
+    return document.getElementById("blogs-link")
+}
+
+
+
+
 
 function resetFormInputs() {
     titleInput().value = "";
@@ -42,10 +62,44 @@ function formTemplate() {
         `;
 }
 
+function blogsTemplate() {
+    return `
+    <h3>List Of Blogs</h3>
+    <div id="blogs">
+
+    </div>
+    `;
+}
+
+function renderBlog(blog) {
+    let div = document.createElement("div");
+    let h4 = document.createElement("h4");
+    let p = document.createElement("p");
+    let blogsDiv = document.getElementById("blogs");
+
+    h4.innerText = `Title: ${blog.title}`;
+    p.innerText = `Content: ${blog.content}`;
+
+    div.appendChild(h4);
+    div.appendChild(p);
+
+    blogsDiv.appendChild(div);
+}
+
 function renderForm() {
     resetMain();
     main().innerHTML = formTemplate();
     form().addEventListener("submit", submitForm);
+}
+
+function renderBlogs() {
+    resetMain();
+    main().innerHTML = blogsTemplate();
+
+    blogs.forEach(function (blog) {
+        renderBlog(blog);
+    });
+
 }
 
 function submitForm(e) {
@@ -57,12 +111,28 @@ function submitForm(e) {
         content: contentInput().value,
     });
 
-    resetFormInputs();
+    renderBlogs();
 }
 
+function formLinkEvent() {
+    formLink().addEventListener("click", function(e) {
+        e.preventDefault();
 
+        renderForm();
+    });
+}
 
+function blogsLinkEvent() {
+    blogsLink().addEventListener("click", function(e) {
+        e.preventDefault();
+
+        renderBlogs();
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     renderForm();
+    formLinkEvent();
+    blogsLinkEvent();
+    //renderBlogs();
 })
